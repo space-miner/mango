6ce731c484704aa41d935d079290ef0f870a1939
space miner <spaceminer@protonmail.com>
2020-10-22 20:42:26 -0400

    add functionality for saving images

--- utils.py
+++ utils.py
@@ -1,8 +1,25 @@
 import subprocess
+import urllib.request
+
+
+def get_pages(page_url_list):
+    """
+    Save manga pages to directory.
+
+    Args:
+        page_url_list: list of direct image links 
+
+    Returns:
+        None.
+    """
+    for page_url in page_url_list:
+        filename = page_url.split('/')[-1]
+        urllib.request.urlretrieve(page_url, filename)
+
 
 def convert(ch):
     """
-    # uses ImageMagick to convert all images in directory to pdf. 
+    Uses ImageMagick to convert all images in directory to pdf. 
 
     Args:
         ch: chapter number.
