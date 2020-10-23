dc5a8172cc8898603aa9de636a82d8dd5135f550
space miner <spaceminer@protonmail.com>
2020-10-22 22:54:28 -0400

    remove get pages

--- utils.py
+++ utils.py
@@ -1,30 +1,27 @@
 import subprocess
-import urllib.request
 
 
-def get_pages(page_url_list):
-    """
-    Save manga pages to directory.
-
-    Args:
-        page_url_list: list of direct image links 
-
-    Returns:
-        None.
-    """
-    for page_url in page_url_list:
-        filename = page_url.split('/')[-1]
-        urllib.request.urlretrieve(page_url, filename)
-
-
-def convert(ch):
+def convert():
     """
     Uses ImageMagick to convert all images in directory to pdf. 
 
     Args:
-        ch: chapter number.
+        None
 
     Returns:
         None.
     """
-    subprocess.run(['convert', '*.png', f'{ch.zfill(4)}.pdf'])
+    subprocess.run(['convert', '*.png', 'out.pdf'])
+
+
+def clean():
+    """
+    Clean directory by removing downloaded images.
+
+    Args:
+        None.
+
+    Returns:
+        None.
+    """
+    subprocess.run(['rm', '*.png'])
