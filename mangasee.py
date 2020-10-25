a7f403bad529e4bedd541a23af2cbb7e2b57ff3e
space miner <spaceminer@protonmail.com>
2020-10-25 02:49:17 -0400

    add image saving functionality

--- mangasee.py
+++ mangasee.py
@@ -1,11 +1,6 @@
 import html
-
-
 from requests_html import HTMLSession
 from bs4 import BeautifulSoup
-
-
-from manga import Manga
 
 
 class MangaSee:
@@ -18,7 +13,7 @@ class MangaSee:
         self.directory = {}
 
 
-    def get_directory(self):
+    def update_directory(self):
         """
         Populates directory, dictionary of title-manga pairs.
 
@@ -28,47 +23,27 @@ class MangaSee:
         Returns:
             None.
         """
-        req = self.session.get(self.directory_url)
-        req.html.render(sleep=5)
-        html = req.html.html
-        soup = BeautifulSoup(html, 'html.parser')
+        soup = self.soupify(self.directory_url)
         data = soup.find_all('a', {'class':'ttip ng-binding'})
         for series in data:
-            title = series.string
-            manga = title.lower()
+            title = series.string.lower()
+            title_id = series.string
             manga_id = series['href'].split('/')[-1]
             manga_url = f"{self.base_url}{series['href']}"
             manga_html = series['title']
             manga_soup = BeautifulSoup(manga_html, 'html.parser')
             cover_url = manga_soup.img['src']
-            self.directory[manga] = Manga(
-                    title, 
-                    manga_id, 
-                    manga_url,
-                    cover_url)
+            self.directory[title] = {
+                    'title id': title_id, 
+                    'manga id': manga_id, 
+                    'manga url': manga_url,
+                    'cover url': cover_url,
+                    'chapters': {}}
 
 
-    def get_manga(self, manga):
+    def get_chapter(self, title, ch):
         """
-        Populates chapters with chapter and the chapter url pairs.
-
-        Args:
-            None.
-
-        Returns:
-            None.
-        """
-        req = self.session.get(manga.manga_url)
-        req.html.render(sleep=5)
-        html = req.html.html
-        soup = BeautifulSoup(html, 'html.parser')
-        # need to populate self.chapters
-        # self.chapters = {}
-
-
-    def get_chapter(self, manga, ch):
-        """
-        Populates chapters[ch]['pages'] with a list of page urls.
+        Populates chapter pages with direct link to page image.
         
         Args:
             ch: chapter number.
@@ -76,14 +51,38 @@ class MangaSee:
         Returns:
             None.
         """
-        chapter_url = f'{self.base_url}/read-online/{manga.manga_id}-chapter-{ch}.html'
-        self.chapter[ch]['url'] = chapter_url
-        
-        req = self.session.get(chapter_url)
-        req.html.render(sleep=5)
-        html = req.html.html
-        soup = BeautifulSoup(html, 'html.parser')
-        self.chapter[ch]['pages'] = [page['src'] for page in soup.find_all('img',
+        manga = self.directory[title]
+        manga_id = manga['manga id']
+        chapter_url = f'{self.base_url}/read-online/{manga_id}-chapter-{ch}.html'
+        soup = self.soupify(chapter_url)
+        manga['chapters'][ch] = [page['src'] for page in soup.find_all('img',
             {'class':'img-fluid'})]
+        for url in manga['chapters'][ch]:
+            self.save_image(url)
 
 
+    def soupify(self, url):
+        for i in range(3):
+            try:
+                print(f"Pulling: {url}")
+                req = self.session.get(url)
+                req.html.render(sleep=5)
+                html = req.html.html
+                return BeautifulSoup(html, 'html.parser')
+            except:
+                print('Failed network request | Retrying')
+
+
+    def save_image(self, url):
+        print(f"Pulling: {url}")
+        req = self.session.get(url)
+        filename = url.split('/')[-1]
+        with open(filename, 'wb') as f:
+            f.write(req.content)
+            f.close()
+
+
+if __name__ == '__main__':
+    ms = MangaSee()
+    ms.update_directory()
+    ms.get_chapter('one piece', 322)
